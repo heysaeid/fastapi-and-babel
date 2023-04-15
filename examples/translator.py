337b3a68c0559a32c2f8214101be0b4c054a8d39
heysaeid <heysaeid92@gmail.com>
2023-04-15 23:00:11 +0330

    Add examples

---
+++ examples/translator.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi_and_babel.translator import FastAPIAndBabel
+
+
+app = FastAPI()
+translator = FastAPIAndBabel(app, "de", "messages")
+
+@app.get("/")
+def index():
+    return {"text": translator.gettext("Hello World!")}
